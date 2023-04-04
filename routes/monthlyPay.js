32dcd6e009d429318b7f66a3f50d5f640e70ccde
Alabi <alabiemmanuel177@gmail.com>
2023-04-04 21:07:27 +0100

    Fixes V1

--- routes/monthlyPay.js
+++ routes/monthlyPay.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const MonthlyPay = require("../models/MonthlyPay");
 const Attendance = require("../models/Attendance");
+const Employee = require("../models/Employee");
 
 // Route to get monthly pay for an employee
 router.get("/:employeeId/:month/:year", async (req, res) => {
@@ -72,6 +73,112 @@ router.get("/:employeeId", async (req, res) => {
     ).populate("employee");
 
     res.json(monthlyPay);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Route to get monthly pay for all employees
+router.get("/", async (req, res) => {
+  try {
+    const now = new Date();
+    const currentMonth = now.getMonth();
+    const currentYear = now.getFullYear();
+    const startOfMonth = new Date(currentYear, currentMonth, 1);
+    const endOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
+    // Find all employees
+    const employees = await Employee.find();
+
+    // Calculate total pay for each employee in the current month
+    const monthlyPays = await Promise.all(
+      employees.map(async (employee) => {
+        // Find all attendances for the employee within the current month and year
+        const attendances = await Attendance.find({
+          employee: employee._id,
+          date: { $gte: startOfMonth, $lte: endOfMonth },
+        });
+
+        // Calculate total pay for the month
+        const totalPay = attendances.reduce(
+          (acc, attendance) => acc + attendance.pay,
+          0
+        );
+
+        // Create or update the MonthlyPay record for the employee and month
+        const monthName = now.toLocaleString("default", { month: "long" });
+        const monthlyPay = await MonthlyPay.findOneAndUpdate(
+          { employee: employee._id, month: monthName, year: currentYear },
+          {
+            employee: employee._id,
+            month: monthName,
+            year: currentYear,
+            totalPay,
+          },
+          { upsert: true, new: true }
+        ).populate("employee");
+
+        return monthlyPay;
+      })
+    );
+
+    res.json(monthlyPays);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Route to get monthly pay for all employees
+router.post("/all", async (req, res) => {
+  try {
+    const now = new Date();
+    const currentMonth = now.getMonth();
+    const currentYear = now.getFullYear();
+    const startOfMonth = new Date(currentYear, currentMonth, 1);
+    const endOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
+    // Find all employees
+    const employees = await Employee.find();
+
+    // Calculate total pay for each employee in the current month
+    const monthlyPays = await Promise.all(
+      employees.map(async (employee) => {
+        // Find all attendances for the employee within the current month and year
+        const attendances = await Attendance.find({
+          employee: employee._id,
+          date: { $gte: startOfMonth, $lte: endOfMonth },
+        });
+
+        // Calculate total pay for the month
+        const totalPay = attendances.reduce(
+          (acc, attendance) => acc + attendance.pay,
+          0
+        );
+
+        // Create or update the MonthlyPay record for the employee and month
+        const monthName = now.toLocaleString("default", { month: "long" });
+        const monthlyPay = await MonthlyPay.findOneAndUpdate(
+          { employee: employee._id, month: monthName, year: currentYear },
+          {
+            employee: employee._id,
+            month: monthName,
+            year: currentYear,
+            totalPay,
+          },
+          { upsert: true, new: true }
+        ).populate("employee");
+
+        return monthlyPay;
+      })
+    );
+    const totalMonthlyPay = monthlyPays.reduce(
+      (acc, monthlyPay) => acc + monthlyPay.totalPay,
+      0
+    );
+
+    res.json({ monthlyPays, totalMonthlyPay });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });

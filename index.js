306124a49e851365dc7a33f647676c8c7ba48586
Alabi <alabiemmanuel177@gmail.com>
2023-04-07 08:39:36 +0100

    CORS

--- index.js
+++ index.js
@@ -8,19 +8,6 @@ const io = require("socket.io")(server, { cors: { origin: "*" } });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const whitelist = ["http://localhost:3000"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
-
 const { routes } = require("./routes/main");
 
 // Registers routes
--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const allowedOrigins = ["http://localhost:3000", "http://example.com"];
+
+// Middleware to implement CORS with specific origins
+const corsMiddleware = function (req, res, next) {
+  const origin = req.headers.origin;
+  if (allowedOrigins.indexOf(origin) !== -1) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+};
+
+// Apply middleware to all routes
+router.use(corsMiddleware);
+
+// Import route handlers
 const AuthRouter = require("./auth");
 const AdminRouter = require("./admin");
 const EmployeeRouter = require("./employee");
@@ -7,15 +29,14 @@ const MonthlyPayRouter = require("./monthlyPay");
 const MonthlyPayrollRouter = require("./monthlyPayroll");
 const AttendanceRouter = require("./attendance");
 
-const routes = ({ app }) => {
-  app.use("/auth", AuthRouter);
-  app.use("/admin", AdminRouter);
-  app.use("/employee", EmployeeRouter);
-  app.use("/position", PositionRouter);
-  app.use("/attendance", AttendanceRouter);
-  app.use("/lateness", LatenessRouter);
-  app.use("/monthlypay", MonthlyPayRouter);
-  app.use("/monthlypayroll", MonthlyPayrollRouter);
-};
+// Register routes
+router.use("/auth", AuthRouter);
+router.use("/admin", AdminRouter);
+router.use("/employee", EmployeeRouter);
+router.use("/position", PositionRouter);
+router.use("/attendance", AttendanceRouter);
+router.use("/lateness", LatenessRouter);
+router.use("/monthlypay", MonthlyPayRouter);
+router.use("/monthlypayroll", MonthlyPayrollRouter);
 
 module.exports = { routes };

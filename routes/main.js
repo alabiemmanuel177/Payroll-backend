9821d84b7a301b336c2c2cb831c617e0ea398f34
Alabi <alabiemmanuel177@gmail.com>
2023-04-07 08:46:23 +0100

    CORS FIXED

--- routes/main.js
+++ routes/main.js
@@ -1,24 +1,3 @@
-const express = require("express");
-const router = express.Router();
-
-const allowedOrigins = ["http://localhost:3000", "http://example.com"];
-
-// Middleware to implement CORS with specific origins
-const corsMiddleware = function (req, res, next) {
-  const origin = req.headers.origin;
-  if (allowedOrigins.indexOf(origin) !== -1) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-};
-
-// Apply middleware to all routes
-router.use(corsMiddleware);
-
 // Import route handlers
 const AuthRouter = require("./auth");
 const AdminRouter = require("./admin");
@@ -29,14 +8,17 @@ const MonthlyPayRouter = require("./monthlyPay");
 const MonthlyPayrollRouter = require("./monthlyPayroll");
 const AttendanceRouter = require("./attendance");
 
+
+const routes = ({ app }) => {
 // Register routes
-router.use("/auth", AuthRouter);
-router.use("/admin", AdminRouter);
-router.use("/employee", EmployeeRouter);
-router.use("/position", PositionRouter);
-router.use("/attendance", AttendanceRouter);
-router.use("/lateness", LatenessRouter);
-router.use("/monthlypay", MonthlyPayRouter);
-router.use("/monthlypayroll", MonthlyPayrollRouter);
+app.use("/auth", AuthRouter);
+app.use("/admin", AdminRouter);
+app.use("/employee", EmployeeRouter);
+app.use("/position", PositionRouter);
+app.use("/attendance", AttendanceRouter);
+app.use("/lateness", LatenessRouter);
+app.use("/monthlypay", MonthlyPayRouter);
+app.use("/monthlypayroll", MonthlyPayrollRouter);
+}
 
 module.exports = { routes };

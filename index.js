81c180484d5e3de5dbaac06c2e6cbe81ba685ba0
Alabi <alabiemmanuel177@gmail.com>
2023-04-07 09:33:53 +0100

    CORS TEST V2

--- index.js
+++ index.js
@@ -7,8 +7,30 @@ const io = require("socket.io")(server, { cors: { origin: "*" } });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const fs = require("fs");
+
+const UPLOADS_DIR = "./uploads";
+
+// Check if the uploads directory exists
+if (!fs.existsSync(UPLOADS_DIR)) {
+  // If it doesn't exist, create the directory
+  fs.mkdirSync(UPLOADS_DIR);
+  console.log("Folder Created");
+} else {
+  console.log("Folder Exist");
+}
 
 const { routes } = require("./routes/main");
+
+// Add the following middleware to enable CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Registers routes
 routes({ app, io });
--- routes/main.js
+++ routes/main.js
@@ -6,102 +6,15 @@ const LatenessRouter = require("./lateness");
 const MonthlyPayRouter = require("./monthlyPay");
 const MonthlyPayrollRouter = require("./monthlyPayroll");
 const AttendanceRouter = require("./attendance");
-const cors = require("cors");
 
-const routes = ({ app, io, allowedOrigins }) => {
-  app.use(
-    cors({
-      origin: function (origin, callback) {
-        // Check if the request origin is in the allowed origins array
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-    })
-  );
-
+const routes = ({ app }) => {
   app.use("/auth", AuthRouter);
   app.use("/admin", AdminRouter);
-  // Add CORS middleware to the LatenessRouter
-  EmployeeRouter.use(function (req, res, next) {
-    const origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
   app.use("/employee", EmployeeRouter);
-  // Add CORS middleware to the LatenessRouter
-  PositionRouter.use(function (req, res, next) {
-    const origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
   app.use("/position", PositionRouter);
-  // Add CORS middleware to the LatenessRouter
-  AttendanceRouter.use(function (req, res, next) {
-    const origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
   app.use("/attendance", AttendanceRouter);
-
-  // Add CORS middleware to the LatenessRouter
-  LatenessRouter.use(function (req, res, next) {
-    const origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
   app.use("/lateness", LatenessRouter);
-  // Add CORS middleware to the MonthlyPayrollRouter
-  MonthlyPayRouter.use(function (req, res, next) {
-    const origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-  app.use("/monthlypay", MonthlyPayrollRouter);
-  // Add CORS middleware to the MonthlyPayrollRouter
-  MonthlyPayrollRouter.use(function (req, res, next) {
-    const origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+  app.use("/monthlypay", MonthlyPayRouter);
   app.use("/monthlypayroll", MonthlyPayrollRouter);
 };
 
